4b34b7bba6b3c5c15fb406387d6970d1ed93e2d2
polupravda <elena.volovicheva@gmail.com>
2018-09-15 12:05:19 +0200

    home page added

---
+++ js/demo-2.js
@@ -0,0 +1,31 @@
+    // var slideshowDeco = document.getElementsByClassName('.slideshow__deco'),
+    //     slideshowImage = document.getElementsByClassName('.slide__img'),
+    //     slideSide = document.getElementsByClassName('.slide__side'),
+    //     slideNumber = document.getElementsByClassName('.slide__number'),
+    //     slideTitle = document.getElementsByClassName('.slide__title'),
+    //     slideSubtitle = document.getElementsByClassName('.slide__subtitle'),
+    //     tl = new TimelineLite();
+
+    // tl
+    // .from(slideshowDeco, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut})
+    // .from(slideshowImage, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut})
+    // .from(slideSide, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut})
+    // .from(slideNumber, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut})
+    // .from(slideTitle, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut})
+    // .from(slideSubtitle, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut})
+
+    (function($) {
+        var slideshowDeco = $('.slideshow__deco'),
+            slideImg = $('.slide__img'),
+            tl = new TimelineMax();
+    
+        tl
+            .fromTo(slideshowDeco, 0.5, {opacity: 0, scale: 5}, {opacity: 1, scale: 1, ease: SlowMo.easeOut})
+            .fromTo(slideImg, 0.5, {opacity: 0, y: 200}, {opacity: 1, y: 50}, '-=0.1')
+    })(jQuery);
+
+    // tl.pause();
+
+    // linkHome.on('click', function(){
+    //     console.log('fuck');
+    // });
